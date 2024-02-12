55fa9e370c0da1a1333e7b63eb3f1d0ff1de1ad3
Atlantasit <marx.leonard@gmx.de>
2024-02-12 17:16:13 +0100

    more log editor editing

--- dotNES/log_output.cs
+++ dotNES/log_output.cs
@@ -7,17 +7,21 @@ namespace dotNES
 {    
     class log_output
     {
-        public string log_time;
+        private string log_time;
+        private string log_file;
+        public void log_setTime(){
+            DateTime currentTime = DateTime.Now;
+            this.log_time = currentTime.Year.ToString() + "-" + currentTime.Month.ToString() + "-" + currentTime.Day.ToString() + "_" + currentTime.Hour.ToString() + "." + currentTime.Minute.ToString();
+        }
 
-        public void write_nextLine(string message){
+        public void log_create(){
             var outPutDirectory = Path.GetDirectoryName(Assembly.GetExecutingAssembly().CodeBase);
             string newLOGPath = outPutDirectory;
             string newlog_file = new Uri(newLOGPath).LocalPath;
+        }
 
-            DateTime currentTime = DateTime.Now;
-            log_time = currentTime.Year.ToString() + "-" + currentTime.Month.ToString() + "-" + currentTime.Day.ToString() + "_" + currentTime.Hour.ToString() + "." + currentTime.Minute.ToString();
-
-            using (StreamWriter writer = new StreamWriter(newlog_file + "\\"+"log_"+ log_time +".txt", true)){
+        public void write_nextLine(string message){
+            using (StreamWriter writer = new StreamWriter(log_file + "\\"+"log_"+ log_time +".txt", true)){
                 writer.WriteLine($"{DateTime.Now} : {message}");
             }
         }
