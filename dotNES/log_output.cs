f55d95610740ca6b2d59f33090b22a30bcf060f4
Atlantasit <marx.leonard@gmx.de>
2024-02-12 12:53:25 +0100

    The logger still need some fixing

--- dotNES/log_output.cs
+++ dotNES/log_output.cs
@@ -7,22 +7,22 @@ namespace dotNES
 {    
     class log_output
     {   
-        int log_time = DateTime.Now;
+        string log_time = DateTime.Now.ToString();
         public void new_log(){
 
             var outPutDirectory = Path.GetDirectoryName(Assembly.GetExecutingAssembly(). CodeBase);
             var newLOGPath = Path.Combine(outPutDirectory, "\\Logs");
-            string newlog_file = new UIntPtr(newLOGPath).localPath;
+            string newlog_file = new Uri(newLOGPath).LocalPath;
             //Creating the new log file
-            using(FileStream fs = File.Create(newLOGPath + "[" + log_time + "]log.txt")){}
+            using(FileStream fs = File.Create(newLOGPath + "\\[" + log_time + "]log.txt")){}
         }
-        
+
         public void write_nextLine(string message){
 
             var outPutDirectory = Path.GetDirectoryName(Assembly.GetExecutingAssembly(). CodeBase);
   
-            var LOGPath = Path.Combine(outPutDirectory,"[" + log_time + "]log.txt");
-            string log_path = new UIntPtr(LOGPath).localPath;
+            var LOGPath = Path.Combine(outPutDirectory,"\\[" + log_time + "]log.txt");
+            string log_path = new Uri(LOGPath).LocalPath;
 
             using(StreamWriter writer = new StreamWriter(log_path,true)){
                 writer.WriteLine($"{DateTime.Now} : {message}");
