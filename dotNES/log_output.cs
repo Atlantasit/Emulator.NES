905dc4a50b87569936795417b92400b74a2bfc3c
Atlantasit <marx.leonard@gmx.de>
2024-02-12 18:22:56 +0100

    Logger now creates folder "Logs"
    but only when the Folder is not present in the curred directory

--- dotNES/log_output.cs
+++ dotNES/log_output.cs
@@ -8,7 +8,7 @@ namespace dotNES
     class log_output
     {
         private string log_time;
-        private string log_file;
+        private string log_path;
         private void log_setTime(){
             DateTime currentTime = DateTime.Now;
             this.log_time = currentTime.Year.ToString() + "-" + currentTime.Month.ToString() + "-" + currentTime.Day.ToString() + "_" + currentTime.Hour.ToString() + "." + currentTime.Minute.ToString();
@@ -19,11 +19,16 @@ namespace dotNES
 
             var outPutDirectory = Path.GetDirectoryName(Assembly.GetExecutingAssembly().CodeBase);
             string newLOGPath = outPutDirectory;
-            string newlog_file = new Uri(newLOGPath).LocalPath;
+            this.log_path = new Uri(newLOGPath).LocalPath + @"\Logs";
+
+            if (!Directory.Exists(log_path)) {
+                Directory.CreateDirectory(log_path);
+            }
+            write_nextLine("New log file created");
         }
 
         public void write_nextLine(string message){
-            using (StreamWriter writer = new StreamWriter(log_file + "\\"+"log_"+ log_time +".txt", true)){
+            using (StreamWriter writer = new StreamWriter(log_path + "\\"+"log_"+ log_time +".txt", true)){
                 writer.WriteLine($"{DateTime.Now} : {message}");
             }
         }
